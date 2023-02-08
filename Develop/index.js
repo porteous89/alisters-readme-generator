278c85ed1edee38d1a340878f2947bce829dd622
Alister Porteous <alisterporteous@hotmail.com>
2023-02-08 09:00:11 -0500

    added functions in generatemarkdown, render badge, choose badge for selected license and link, added function to generate readme from answers. in index added list of question prompts, write file function to generate readme from markdown

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,64 @@
 // TODO: Include packages needed for this application
-test
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {   type: 'input', 
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {   type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project.'
+    },
+    {   type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions.'
+    },
+    {   type: 'input',
+        name: 'usage',
+        message: 'Please provide usage information.'
+    },
+    {   type: 'input',
+        name: 'contribution',
+        message: 'Please provide contribution guidelines.'
+    },
+    {   type: 'input',
+        name: 'test',
+        message: 'Please provide test instructions.'
+    },
+    {   type: 'list',
+        name: 'license',
+        message: 'Please select a license.',
+        choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None']
+    },
+    {   type: 'input',
+        name: 'github',
+        message: 'Please provide your GitHub username.'
+    },
+    {   type: 'input',
+        name: 'email',
+        message: 'Please provide your email address.'
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.log(err) : console.log('Successfully generated README file!')
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((data) => {
+            writeToFile('README.md', generateMarkdown(data));
+        });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,79 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return '';
+  }
+
+  switch (license) {
+    case 'MIT':
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    case 'Apache 2.0':
+      return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    case 'GPL 3.0':
+      return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  }
+
+  switch (license) {
+    case 'MIT':
+      return 'https://opensource.org/licenses/MIT';
+    case 'Apache 2.0':
+      return 'https://opensource.org/licenses/Apache-2.0';
+    case 'GPL 3.0':
+      return 'https://www.gnu.org/licenses/gpl-3.0';
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  }
+
+  return `
+This project is licensed under the ${license} license. See the [LICENSE](${renderLicenseLink(license)}) file for details.
+
+${renderLicenseBadge(license)}`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `
+  ${renderLicenseBadge(data.license)}
+# ${data.title}
+## Description
+${data.description}
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## License
+${renderLicenseSection(data.license)}
+## contribution
+${data.contribution}
+## Test
+${data.test}
+## Questions
+If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.github}](${data.github}).
 `;
 }
 
